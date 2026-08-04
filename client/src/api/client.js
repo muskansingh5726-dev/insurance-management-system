2600d7e93e18ed3adea1bd686a8c6dbdaa52a1e5
Muskan Singh <muskansingh5726@gmail.com>
2026-08-04 18:24:41 +0530

    Fix API URL

--- client/src/api/client.js
+++ client/src/api/client.js
@@ -1,14 +1,18 @@
-import axios from 'axios';
+import axios from "axios";
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL:
+    import.meta.env.VITE_API_URL ||
+    "https://insurance-management-system-jhrj.onrender.com/api"
 });
 
 API.interceptors.request.use((config) => {
-  const token = localStorage.getItem('token');
+  const token = localStorage.getItem("token");
+
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
+
   return config;
 });
 
